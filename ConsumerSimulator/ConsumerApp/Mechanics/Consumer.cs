353830b4b20af24f6e3a512bd7668792577ad099
MoJeez <kenethanders@gmail.com>
2021-12-31 15:41:47 +0100

    Added Consumer class
    Made the consumer ready for test

---
+++ ConsumerSimulator/ConsumerApp/Mechanics/Consumer.cs
@@ -0,0 +1,31 @@
+﻿using ConsumerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsumerApp.Mechanics
+{
+    public class Consumer
+    {
+        public Stack<Item> Items { get; set; }
+        public Item Item { get; set; }
+        public bool Running { get; set; }
+        public async Task<bool> StartConsumer()
+        {
+            Running = true;
+            Item = await Consume();
+            Running = false;
+            return true;
+        }
+
+        private async Task<Item> Consume()
+        {
+            Task<Item> item = Task<Item>.Factory.StartNew(() =>
+            {
+                Task.Delay(100);
+                return Items.Pop();
+            });
+
+            return await Task.FromResult(await item);
+        }
+    }
+}
