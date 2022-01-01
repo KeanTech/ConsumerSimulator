34128942088f501840aae3aebd3086114637d65e
MoJeez <kenethanders@gmail.com>
2022-01-01 12:36:25 +0100

    Added Tests for Producer
    Added Tests for Produce and StartProducer

---
+++ ConsumerSimulator/ConsumerSimulatorTests/ProducerTests.cs
@@ -0,0 +1,30 @@
+﻿using ConsumerApp.Mechanics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsumerSimulatorTests
+{
+    public class ProducerTests
+    {
+        [Fact]
+        public async void Produce_ShouldReturnNewItem()
+        {
+            Producer producer = new();
+
+            await producer.StartProducer();
+            Assert.NotEmpty(producer.Items);
+        }
+
+        [Fact]
+        public async void StartProducer_ShouldReturnTrue()
+        {
+            Producer producer = new();
+
+            Assert.True(await producer.StartProducer());
+        }
+    }
+}
